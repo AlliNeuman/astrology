d59a41c3b431d7f9e4cbb8db4294222eb6960b90
Allison Tzadok <allisontzadok@gmail.com>
2018-02-01 23:05:04 +0000

    added menu and goodbye methods from other repo that was set up with only the horoscope and cli, no scraper class

--- lib/astrology/cli.rb
+++ lib/astrology/cli.rb
@@ -14,9 +14,32 @@ class Astrology::Cli
   end
 
   def menu
+    input = nil
+    while input != "exit"
 
+      puts "To see your horoscope, enter the number 1-12 that corresponds with your astrological sign, type 'list' to see the list of signs or type 'exit'"
+      input = gets.strip.downcase
+      if input.to_i > 0
+        horoscope = Astrology::Horoscope.all[input.to_i - 1]
+        puts "#{@headline}"
+        puts "#{horoscope.name}"
+        puts "#{horoscope.dates}"
+        puts "#{horoscope.summary}"
+        puts "Lucky Number: #{horoscope.lucky_number}"
+        puts "Financial Outlook: #{horoscope.financial_outlook}"
+        puts "Partner Compatibility: #{horoscope.compatible_sign}"
+
+
+      elsif input == "list"
+        list_horoscopes
+      else
+        "not sure what you want, type 'list' or 'exit'"
+      end
+    end
   end
 
-
+  def goodbye
+    puts "check back again for your daily horoscope!"
+  end
 
 end
